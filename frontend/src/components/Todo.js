b3f5fc9b6cab53cb03f033ed8bc5ad2bd8704e0b
JNSREDDY <jsurendrareddy786@gmail.com>
2026-02-10 23:36:16 +0530

    Updated frontend API URL for production

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,82 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+const BASE_URL = "https://todolist-4dck.onrender.com";
 
-function Todo() {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
+const fetchTodos = async () => {
+  const res = await axios.get(BASE_URL);
+  setTodos(res.data);
+};
 
-  const fetchTodos = async () => {
-    const res = await axios.get("http://localhost:5000/api/todos");
-    setTodos(res.data);
-  };
+const addTask = async () => {
+  if (!task) return;
+  await axios.post(BASE_URL, { task });
+  setTask("");
+  fetchTodos();
+};
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+const deleteTask = async (id) => {
+  await axios.delete(`${BASE_URL}/${id}`);
+  fetchTodos();
+};
 
-  const addTask = async () => {
-    if (!task) return;
-    await axios.post("http://localhost:5000/api/todos", { task });
-    setTask("");
-    fetchTodos();
-  };
-
-  const deleteTask = async (id) => {
-    await axios.delete(`http://localhost:5000/api/todos/${id}`);
-    fetchTodos();
-  };
-
-  const toggleTask = async (id) => {
-    await axios.put(`http://localhost:5000/api/todos/${id}`);
-    fetchTodos();
-  };
-
-  return (
-    <div>
-      <div className="input-section">
-        <input
-          type="text"
-          placeholder="Enter task..."
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <button className="add-btn" onClick={addTask}>
-          Add
-        </button>
-      </div>
-
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span
-              className="task-text"
-              style={{
-                textDecoration: todo.completed ? "line-through" : "none",
-              }}
-            >
-              {todo.task}
-            </span>
-
-            <div className="btn-group">
-              <button
-                className="done-btn"
-                onClick={() => toggleTask(todo.id)}
-              >
-                Done
-              </button>
-
-              <button
-                className="delete-btn"
-                onClick={() => deleteTask(todo.id)}
-              >
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default Todo;
+const toggleTask = async (id) => {
+  await axios.put(`${BASE_URL}/${id}`);
+  fetchTodos();
+};
